aaff8f88fe7a14cccefbb03cd14c1fb128e88c0a
Rohit Roushan <rohitroushan@Rohits-Laptop.local>
2026-07-10 20:56:17 +0530

    container with most water

---
+++ misc/src/main/java/misc/ContainerWithMostWater.java
@@ -0,0 +1,30 @@
+package misc;
+
+public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxWater(height));
+    }
+
+    private static int maxWater(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while(left < right){
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if(height[left] < height[right]){
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
