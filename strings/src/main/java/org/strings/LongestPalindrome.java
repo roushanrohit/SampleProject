6fd45d3bd40bb42ef76bfdf22aaaa2784a2dd366
Rohit Roushan <rohitroushan@Rohits-Laptop.local>
2026-07-10 09:01:45 +0530

    Longest Palindrome v1

--- strings/src/main/java/org/strings/LongestPalindrome.java
+++ strings/src/main/java/org/strings/LongestPalindrome.java
@@ -7,9 +7,37 @@ public class LongestPalindrome {
         System.out.println(longestPalindrome("babad"));
     }
 
-    private static String longestPalindrome(String babad) {
+    private static String longestPalindrome(String s) {
 
+        int maxLength = 0;
+        int si = -1;
+        int ei = -1;
+        for(int i = 0; i < s.length(); i++){
+            for(int j = s.length(); j > i; j--){
+                int palindromeLength = checkPalindrome(s.substring(i, j));
+                if(palindromeLength > 0){
+                    // we have found the longest palindrome starting at i
+                    if(palindromeLength > maxLength){
+                        maxLength = palindromeLength;
+                        si = i;
+                        ei = j;
+                    }
+                    break;
+                }
+            }
+        }
+        return s.substring(si, ei);
+    }
 
-        return "";
+    private static int checkPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return 0;
+            }
+            i++;
+            j--;
+        }
+        return s.length();
     }
 }
