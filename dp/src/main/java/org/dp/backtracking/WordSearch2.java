d30203ac6513dabda969dcce3f6814bc588238b8
Rohit Roushan <rohitroushan@Rohits-Laptop.local>
2026-07-17 23:44:37 +0530

    word search 2

---
+++ dp/src/main/java/org/dp/backtracking/WordSearch2.java
@@ -0,0 +1,77 @@
+package org.dp.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordSearch2 {
+
+    static int[][] dir = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+    };
+
+    public static void main(String[] args) {
+
+        char[][] grid = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String[] words = {"ABCCED"};
+        System.out.println(findWords(grid, words));
+    }
+
+
+    private static List<String> findWords(char[][] board, String[] words) {
+
+        List<String> ans = new ArrayList<>();
+        for(String word : words){
+            if(findWord(board, word)){
+                ans.add(word);
+            }
+        }
+        return ans;
+    }
+
+    private static boolean findWord(char[][] grid, String word) {
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(findWord(grid, word, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean findWord(char[][] grid, String word, int i, int j, int pos) {
+
+        // found all the characters
+        if(pos == word.length()){
+            return true;
+        }
+        if (i < 0 || j < 0 || i == grid.length || j == grid[0].length) {
+            return false;
+        }
+        if (!(grid[i][j] == word.charAt(pos))) {
+            return false;
+        }
+
+        // mark that this character has been visited -- do not visit again
+        char ch = grid[i][j];
+        // '\0' is the null character. It is a character literal whose Unicode value is 0
+        grid[i][j] = '\0';
+
+        for(int[] d : dir){
+            if(findWord(grid, word, i + d[0], j + d[1], pos + 1)) {
+                // restore state
+                grid[i][j] = ch;
+
+                return true;
+            }
+        }
+
+        // restore state
+        grid[i][j] = ch;
+
+        // word not found
+        return false;
+    }
+}
