015e9d53a7cf0f0626a153b28252650293526a18
Rohit Roushan <rohitroushan@Rohits-Laptop.local>
2026-09-03 17:01:32 +0530

    median of two sorted arrays modified for different length arrays

--- binarysearch/src/main/java/binarysearch/MedianOfTwoSortedArrays.java
+++ binarysearch/src/main/java/binarysearch/MedianOfTwoSortedArrays.java
@@ -4,28 +4,50 @@ public class MedianOfTwoSortedArrays {
 
     public static void main(String[] args) {
 
-        int[] a = {1, 2, 3, 10};
-        int[] b = {4, 5, 6, 7};
-        int n = a.length;
-        System.out.println(medianOfSortedArrays(a, b, n));
+        int[] a = {1,2};
+        int[] b = {3,4};
+        System.out.println(medianOfSortedArrays(a, b));
     }
 
-    private static double medianOfSortedArrays(int[] a, int[] b, int n) {
+    private static double medianOfSortedArrays(int[] a, int[] b) {
 
+        // Binary search on the smaller array
+        if (a.length > b.length) {
+            return medianOfSortedArrays(b, a);
+        }
+
+        /* n should represent the number of elements that must be on
+           the left side of the partition across both arrays.
+         */
+        int n = (a.length + b.length + 1)/2;
         int left = 0;
-        int right = n;
+        int right = a.length;
         while(left <= right){
-            int i = (left + right)/2;
+            int i = (left + right) / 2;
             int j = n - i;
-            if(a[i - 1] > b[j]){
+
+            int aLeft  = (i == 0) ? Integer.MIN_VALUE : a[i - 1];
+            int aRight = (i == a.length) ? Integer.MAX_VALUE : a[i];
+
+            int bLeft  = (j == 0) ? Integer.MIN_VALUE : b[j - 1];
+            int bRight = (j == b.length) ? Integer.MAX_VALUE : b[j];
+
+            if (aLeft > bRight) {
                 right = i - 1;
-            } else if(b[j - 1] > a[i]){
+            } else if (bLeft > aRight) {
                 left = i + 1;
             } else {
-                // partition is correct
-                int max = Math.max(a[i - 1], b[j - 1]);
-                int min = Math.min(a[i], b[j]);
-                return (min + max)/2.0;
+                // Correct partition
+                int maxLeft = Math.max(aLeft, bLeft);
+                int minRight = Math.min(aRight, bRight);
+
+                // Odd total length
+                if ((a.length + b.length) % 2 == 1) {
+                    return maxLeft;
+                } else {
+                    // Even total length
+                    return (maxLeft + minRight) / 2.0;
+                }
             }
         }
         return 0.0;
