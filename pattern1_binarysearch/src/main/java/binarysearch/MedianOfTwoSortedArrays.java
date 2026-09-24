f52579bc6fda5057f71b2abc47b4a0d36b644a60
Rohit Roushan <rohitroushan@Rohits-Laptop.local>
2026-09-24 18:26:08 +0530

    Median of two sorted arrays - revisited

--- pattern1_binarysearch/src/main/java/binarysearch/MedianOfTwoSortedArrays.java
+++ pattern1_binarysearch/src/main/java/binarysearch/MedianOfTwoSortedArrays.java
@@ -9,41 +9,32 @@ public class MedianOfTwoSortedArrays {
         System.out.println(medianOfSortedArrays(a, b));
     }
 
-    private static double medianOfSortedArrays(int[] a, int[] b) {
+    private static double medianOfSortedArrays(int[] a, int[] b){
 
-        // Binary search on the smaller array
-        if (a.length > b.length) {
-            return medianOfSortedArrays(b, a);
-        }
+        int m = a.length;
+        int n = b.length;
+        int leftSize = (m + n + 1)/2;
 
-        int leftSize = (a.length + b.length + 1)/2;
-        int left = 0;
-        int right = a.length;
+        int left = Math.max(0, leftSize - n);
+        int right = Math.min(leftSize, m);
         while(left <= right){
-            int i = (left + right) / 2;
+            int i = (left + right)/2;
             int j = leftSize - i;
-
-            int aLeft  = (i == 0) ? Integer.MIN_VALUE : a[i - 1];
-            int aRight = (i == a.length) ? Integer.MAX_VALUE : a[i];
-
-            int bLeft  = (j == 0) ? Integer.MIN_VALUE : b[j - 1];
-            int bRight = (j == b.length) ? Integer.MAX_VALUE : b[j];
-
-            if (aLeft > bRight) {
+            int aLeft = (i == 0) ? Integer.MIN_VALUE : a[i - 1];
+            int aRight = (i == m) ? Integer.MAX_VALUE : a[i];
+            int bLeft = (j == 0) ? Integer.MIN_VALUE : b[j - 1];
+            int bRight = (j == n) ? Integer.MAX_VALUE : b[j];
+            // check validity
+            if(aLeft > bRight){
                 right = i - 1;
-            } else if (bLeft > aRight) {
+            } else if(bLeft > aRight){
                 left = i + 1;
             } else {
-                // Correct partition
-                int maxLeft = Math.max(aLeft, bLeft);
-                int minRight = Math.min(aRight, bRight);
-
-                // Odd total length
-                if ((a.length + b.length) % 2 == 1) {
-                    return maxLeft;
+                // valid pair
+                if((m + n) % 2 == 0){
+                    return (Math.max(aLeft, bLeft) + Math.min(aRight, bRight))/2.0;
                 } else {
-                    // Even total length
-                    return (maxLeft + minRight) / 2.0;
+                    return Math.max(aLeft, bLeft);
                 }
             }
         }
